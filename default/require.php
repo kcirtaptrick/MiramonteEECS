5280457c1988c1a698de0470b67781773dc03098
Patrick Wang <patrick.bohan.wang@gmail.com>
2018-11-16 20:45:12 +0000

    changed colors

--- default/require.php
+++ default/require.php
@@ -3,7 +3,7 @@ function root() {
   return dirname(__DIR__);
 }
 function connect() {
-  $serverID = $_ENV['serverID'];
+  // $serverID = $_ENV['serverID'];
   $conn = strpos($_SERVER['HTTP_HOST'], "c9users.io") ? 
     new mysqli(getenv('IP'), getenv('C9_USER'), "", "c9") : 
     new mysqli("mysql.hostinger.com", $serverID . "_admin", $_ENV['serverPW'], $serverID . "_accnt");
