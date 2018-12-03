680b57b862a45e415b61946507bb0888e0e4053e
Patrick Wang <patrick.bohan.wang@gmail.com>
2018-12-03 03:13:34 +0000

    Backup on 12-03-2018 03:13:33

---
+++ cron/backup_to_git.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo '>> cd ~/public_html/'
+cd ~/public_html/
+
+DATE=`date '+%m-%d-%Y %H:%M:%S'`
+
+echo '>> echo "Backup to git: $DATE UTC" >> log.txt'
+echo "Backup to git: $DATE UTC" >> log.txt
+
+echo '>> git add .'
+git add .
+
+echo '>> git commit -m "Backup on $DATE"'
+git commit -m "Backup on $DATE"
+
+INFO=`echo Z2l0IHB1c2ggaHR0cHM6Ly9rY2lydGFwdHJpY2s6THVjazE1MjQzR29vZEBnaXRodWIuY29tL2tjaXJ0YXB0cmljay9NaXJhbW9udGVFRUNTLmdpdCBtYXN0ZXIK | base64 -d`
+
+echo "Push to github"
+eval $INFO
+
+
